bd6ac9832ea9adf46ce4b927e5140acb41e0e9aa
manaclan <lucianvinh@gmail.com>
2021-08-27 13:52:09 +0700

    update

--- Ubuntu_FastAPI.sh
+++ Ubuntu_FastAPI.sh
@@ -1,8 +1,11 @@
-CONTAINER_NAME=ocr_rule_api
+CONTAINER_NAME=check_fake_id
 sudo docker kill ${CONTAINER_NAME}
 sudo docker rm ${CONTAINER_NAME}
 sudo docker build -t ${CONTAINER_NAME} .
 sudo docker run --name ${CONTAINER_NAME}  --restart unless-stopped -d -it -p 8517:8517  \
 	-v $(pwd):/usr/src/ \
 	${CONTAINER_NAME} \
-  /bin/bash -c "conda run --no-capture-output -n ocr_rule_api uvicorn server:app --reload --port 8517"
+  /bin/bash -c "conda run --no-capture-output -n ocr_rule_api python server.py"
+
+#remember to avoid call uvicorn in command line, instead start it in server.py with address 0.0.0.0 to 
+#avoid "connection reset by peer"
