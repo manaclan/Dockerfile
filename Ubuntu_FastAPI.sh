61b5d14c8bb61b699d947a61e28ea75885cef8a1
manaclan <lucianvinh@gmail.com>
2021-08-28 16:01:45 +0700

    update

--- Ubuntu_FastAPI.sh
+++ Ubuntu_FastAPI.sh
@@ -7,5 +7,9 @@ sudo docker run --name ${CONTAINER_NAME}  --restart unless-stopped -d -it -p 851
 	${CONTAINER_NAME} \
   /bin/bash -c "conda run --no-capture-output -n ocr_rule_api python server.py"
 
-#remember to avoid call uvicorn in command line, instead start it in server.py with address 0.0.0.0 to 
+# note1: remember to avoid call uvicorn in command line, instead start it in server.py with address 0.0.0.0 to 
 #avoid "connection reset by peer"
+
+#note2: when using fast with uvicorn, remember to python3 -m pip install uvicorn[standard]
+#or else: when mapping to domain client call to it will received "Unsupported upgrade request"
+#https://github.com/tiangolo/fastapi/issues/2209
